10af0b9f7ba21c6a5e28f99ff0349bf188a6c6cb
Michael V. Battista <michael.v.battista@gmail.com>
2014-08-03 15:08:34 -0400

    Cleanup creator page from pair programming

--- creator/creator.js
+++ creator/creator.js
@@ -1,5 +1,5 @@
-var app = angular.module('jeopardy',[]);
-    app.controller('MainCtrl', function($scope){
+var app = angular.module('jeopardy-creator',[]);
+    app.controller('CreatorCtrl', function($scope){
         $scope.main = {};
         
         $scope.board = {
@@ -8,32 +8,32 @@ var app = angular.module('jeopardy',[]);
           "double-jeopardy": [
           ],
           "final-jeopardy": {
-              "answer": "",
-              "category": "",
-              "question": ""
+              "answer": "Who is some guy in Normandy But I just won $75,000!",
+              "category": "MILITARY MEN",
+              "question": "On June 6, 1944 he said, \"The eyes of the world are upon you\""
           },
         };
-        
+
         $scope.load = function(){
             $scope.board = JSON.parse($scope.pre);
         };
         $scope.addCategory = function(round){
             round.push({
-                name:"Category",
+                name:"AN ALBUM COVER",
                 questions:[]
             });
         };
         $scope.addQuestion = function(category){
             category.questions.push({
-                question:"WHAT ALEX SAYS",
+                question:"THE BEATLES WHITE ALBUM IS THIS COLOR.",
                 value:0,
-                answer:"What is a ...?"
+                answer:"Who are the Beatles?"
             });
         };
         $scope.addImage = function(question) {
-          question.image = "";          
+          question.image = "";
         };
         $scope.removeImage = function(question) {
-          delete question.image;          
+          delete question.image;
         };
     });
